d6577aae71d038a2403def2e9d1ffe77d6e969bb
Florian Koch <florat.min@gmail.com>
2023-11-07 19:01:08 +0100

    remove unneeded index.ts

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export {connect as connectDoesNotWork1} from './test-works/index';
-export {connect as connectDoesNotWork2} from './test-works-2/index';
-export {connect} from './test-does-not-work/index';
